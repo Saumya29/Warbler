4aed37e4a8eaf1df0adfeaa009c2085ccd2f3e33
Saumya29 <saumyatiwari.29@gmail.com>
2019-03-25 18:22:21 +0530

    Boilerplate set

---
+++ warbler-server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const PORT = 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//routes come here
+
+app.use(function(req, res, next) {
+  let err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.listen(PORT, function() {
+  console.log(`Server listening on PORT ${PORT}`);
+});
