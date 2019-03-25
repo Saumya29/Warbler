eccb65d34942b78a2012fc3bd8616b17faf09517
Saumya29 <saumyatiwari.29@gmail.com>
2019-03-25 18:30:52 +0530

    Added user model and setup mongoose

---
+++ warbler-server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profileImageUrl: {
+    type: String,
+  }
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
